b5b9ecae8c1b805674c1df2a9027c707b152a0ba
Prajotsna-P <77489461+Prajotsna-P@users.noreply.github.com>
2022-04-30 20:25:03 +0000

    Add files via upload

---
+++ FootballController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FootballDbFirst.Models;
+using System.Data;
+
+namespace FootballDbFirst.Controllers
+{
+    public class FootballController : Controller
+    {
+        // GET: Football
+        FootballDbEntities Context = new FootballDbEntities();
+        public ActionResult Index()
+        {
+            List<tblFootballLeague> result = (List<tblFootballLeague>)Context.tblFootballLeagues.Where(x => x.Status == "win").ToList();
+            return View(result);
+        }
+    }
+}
