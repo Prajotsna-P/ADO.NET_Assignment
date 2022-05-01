a18b394b570c6e84acebbfaac7bbb3505eba7db8
Prajotsna-P <77489461+Prajotsna-P@users.noreply.github.com>
2022-05-01 15:32:10 +0000

    Add files via upload

---
+++ BusinfoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Web.Mvc;
+using Businfo_2._3.Models;
+
+
+namespace Businfo_2._3.Controllers
+{
+    public class BusinfoController : Controller
+    {
+        BusinfodbEntities db = new BusinfodbEntities();
+        public ActionResult Index()
+        {
+            return View((from c in db.BusInfoes
+                         where c.BoardingPoint == "MUM"
+                         select c));
+        }
+    }
+}
